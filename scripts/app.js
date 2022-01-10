94d1b7273086198fe92c0f255229abb52a683958
MuhammadUsmanMushtaq <usman.muhammad@codezyme.com>
2022-01-10 02:59:08 +0100

    icons & img added finalise the app

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,45 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  //destructure properties
+  const { cityDets, weather } = data;
+  console.log(data);
+  //update details temlapte
+  details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+  //update day/night & icon img
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+
+  time.setAttribute("src", timeSrc);
+
+  // remove the display none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
 
   return {
-    cityDets: cityDets,
-    weather: weather,
+    //objext shorthand notation when the property name and value has a same name we can use short hand notation
+    cityDets,
+    weather,
   };
 };
 
@@ -19,6 +52,6 @@ cityForm.addEventListener("submit", (e) => {
 
   //updating new city in ui
   updateCity(city)
-    .then((data) => console.log(data))
+    .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 });
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -17,5 +17,6 @@ const getCity = async (city) => {
 
   const response = await fetch(base + query);
   const data = await response.json();
+
   return data[0];
 };
